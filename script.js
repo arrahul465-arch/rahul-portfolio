c631a7cccdd96c9b1491bffca23080ddc479e420
arrahul465-arch <arrahul465@gmail.com>
2025-08-05 15:32:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function sendMail(){
+    var params={
+        name:document.getElementById("name").value ,
+        email:document.getElementById("email").value ,
+        message:document.getElementById("message").value ,
+
+    };
+
+    const serviceID="service_hf3z8de";
+    const templateID="template_kghcdaf";
+    
+    emailjs
+        .send(serviceID,templateID,params)
+        .then(res=>{
+                document.getElementById("name").value="";
+                document.getElementById("email").value="";
+                document.getElementById("message").value="";
+                console.log(res);
+                alert("your message sent successfuly")
+        })
+        .catch((err)=>console.log(err));
+}
